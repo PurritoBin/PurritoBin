d3c2c4b38d87592c06d94129ddb1f1e869832417
Aisha Tammy <purrito@bsd.ac>
2021-01-16 11:36:40 -0500

    move clients to cut instead of set

--- clients/POSIX_shell_client.sh
+++ clients/POSIX_shell_client.sh
@@ -8,28 +8,31 @@
 
 # POSIX shell client to upload standard message
 purr() {
-	curl --silent --data-binary "@${1:-/dev/stdin}" bsd.ac:42069
+        curl --silent --data-binary "@${1:-/dev/stdin}" bsd.ac:42069
 }
-
 
 # POSIX shell client to upload encrypted message
 meow() {
-	# we need to generate a 256 byte random key
-	# for using the aes-256-cbc cipher
-	key="$(openssl rand -hex 32)"
-	iv="$(openssl rand -hex 12)"
-	# calculate its encryption and upload it
-	url="$(openssl enc -aes-256-cbc -K ${key} -iv ${iv} -e -base64 -A < ${1:-/dev/stdin} | purr)"
-	printf %s\\n "${url%\/*}/paste.html#${url##*\/}_${key}_${iv}"
+        # we need to generate a 256 byte random key
+        # for using the aes-256-cbc cipher
+        key="$(openssl rand -hex 32)"
+        iv="$(openssl rand -hex 16)"
+        # calculate its encryption and upload it
+        url="$(openssl enc -aes-256-cbc -K ${key} -iv ${iv} -e -base64 -A < ${1:-/dev/stdin} | purr)"
+        printf %s\\n "${url%\/*}/paste.html#${url##*\/}_${key}_${iv}"
+        unset key iv url
 }
 
 
 # POSIX shell client to decrypt the message
 meowd() {
-	url="$1"
-	baseurl="${url%\/*}"
-	vals="${url##*\#}"
-	IFS="_" set -- $vals
-	encrypteddata="$(curl --silent ${baseurl}/$1)"
-	printf %s\\n $encrypteddata | openssl enc -aes-256-cbc -base64 -d -K $2 -iv $3
+        url="$1"
+        baseurl="${url%\/*}"
+        vals="${url##*\#}"
+        paste=$(printf %s\\n "${vals}" | cut -d'_' -f1)
+        key=$(printf %s\\n "${vals}" | cut -d'_' -f2)
+        iv=$(printf %s\\n "${vals}" | cut -d'_' -f3)
+        encrypteddata="$(curl --silent ${baseurl}/$paste)"
+        printf %s\\n $encrypteddata | openssl enc -aes-256-cbc -base64 -d -K $key -iv $iv
+        unset url baseurl vals paste key iv
 }
