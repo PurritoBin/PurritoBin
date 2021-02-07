624ef9e84c0241e470872b14ef9e20eed1738943
Aisha Tammy <floss@bsd.ac>
2021-02-07 17:14:16 +0000

    add runtime changed perms test
    Signed-off-by: Aisha Tammy <floss@bsd.ac>

---
+++ tests/test_single_paste_runtime_change_perms.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+. ./common.sh
+
+set -e
+
+P_TMPDIR="$(mktemp -d -t)"
+P_PORT="$(${SHUF} -i 1500-65536 -n 1)"
+
+P_RACING=1
+../purrito -d "${P_TMPDIR}/" -s "${P_TMPDIR}" -i 127.0.0.1 -p "${P_PORT}" &
+P_ID=$!
+P_RACING=
+
+# should be enough
+sleep 2
+
+chmod -w "${P_TMPDIR}"
+
+P_DATA="SOME_RANDOM_TEST_DATA"
+
+P_PASTE=$(printf %s\\n "${P_DATA}" | purr)
+
+if [ ! -z "${P_PASTE}" ]; then
+    exit 1
+fi
+
+chmod +w "${P_TMPDIR}"
+
+set +e
+pinfo "${0}: success"
