c850d3ae5758bf8cfb9831b0d36aa3b2f10296a7
Aisha Tammy <floss@bsd.ac>
2021-02-09 13:50:10 +0000

    no *s for vectors
    Signed-off-by: Aisha Tammy <floss@bsd.ac>

--- src/purrito.h
+++ src/purrito.h
@@ -278,7 +278,7 @@ std::string save_buffer(const std::shared_ptr<std::vector<char>> buffer,
   }
 
   int write_count =
-      fwrite((*buffer).data(), sizeof(char), buffer_size, output_file);
+      fwrite(buffer->data(), sizeof(char), buffer_size, output_file);
 
   fclose(output_file);
 
