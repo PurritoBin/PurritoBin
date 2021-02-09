382742bd65e875d47fd9bebfe8a5134268a37688
Aisha Tammy <floss@bsd.ac>
2021-02-09 20:49:33 +0000

    add really large SSL tests
    Signed-off-by: Aisha Tammy <floss@bsd.ac>

--- tests/common.sh
+++ tests/common.sh
@@ -7,11 +7,11 @@ fi
 pinfo()  { printf %s\\n "${__green}${__bold}LOG${__reset}: $*"; }
 
 purr() {
-    curl --max-time "${P_MAXTIME:-10}" --silent --data-binary "@${1:-/dev/stdin}" "localhost:${P_PORT}"
+    curl --max-time "${P_MAXTIME:-30}" --silent --data-binary "@${1:-/dev/stdin}" "localhost:${P_PORT}"
 }
 
 spurr() {
-    curl --max-time "${P_MAXTIME:-10}" --silent --cacert PB.crt --capath "$(pwd)" --data-binary "@${1:-/dev/stdin}" "https://localhost:${P_PORT}"
+    curl --max-time "${P_MAXTIME:-30}" --silent --cacert PB.crt --capath "$(pwd)" --data-binary "@${1:-/dev/stdin}" "https://localhost:${P_PORT}"
 }
 
 trap_exit() {
